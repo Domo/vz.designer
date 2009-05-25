7a6fb79f4802b98efa5e3215c7bf128d81c62dee
Stefan Exner <stex@sterex.de>
2009-05-25 14:27:07 +0100

    confirmation page running now

--- server/app/models/reservation_room_type.rb
+++ server/app/models/reservation_room_type.rb
@@ -2,13 +2,13 @@ class ReservationRoomType
 	require 'database'
 	attr_accessor :adults, :children, :reservation_id, :occupancy, :sell_price, :room_type, :rate
 	def initialize(_reservation)
-		self.adults = rand(4) + 1
-		self.children = rand(2) + 1
+		self.room_type = Database.find(:random, :room_types)
+		self.rate = Database.find(:random, :rates)
+		self.adults = rand(self.room_type.adults) + self.room_type.min_adults
+		self.children = (rand(self.room_type.max_children) + self.room_type.children).to_i
 		self.reservation_id = _reservation.id
 		self.occupancy = self.adults + self.children
 		self.sell_price = 50
-		self.room_type = Database.find(:random, :room_types)
-		self.rate = Database.find(:random, :rates)
 	end
 	
 	def property_room_type
